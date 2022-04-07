d1fd37e02852425e24e294f4e01fe857fd652482
Janne Mattila <janne.mattila@hotmail.fi>
2022-04-07 10:10:54 +0300

    Update demo

--- 00-variables.sh
+++ 00-variables.sh
@@ -22,6 +22,32 @@ resource_group_name="rg-aks-workshop-$my_name"
 # Azure region to use
 location="swedencentral"
 
+#########################
+#                   _
+# __   ___ __   ___| |_
+# \ \ / / '_ \ / _ \ __|
+#  \ V /| | | |  __/ |_
+#   \_/ |_| |_|\___|\__|
+# Virtual Networks vars
+#########################
+
+vnet_hub_name="hub-vnet"
+vnet_hub_address_prefix="10.0.0.0/21"
+vnet_hub_management_subnet_name="snet-management"
+vnet_hub_management_subnet_address_prefix="10.0.3.0/24"
+vnet_hub_bastion_subnet_name="AzureBastionSubnet"
+vnet_hub_bastion_subnet_address_prefix="10.0.4.0/24"
+
+vnet_spoke1_name="vnet-spoke1"
+vnet_spoke1_address_prefix="10.1.0.0/22"
+vnet_spoke1_front_subnet_name="snet-front"
+vnet_spoke1_front_subnet_address_prefix="10.1.0.0/24"
+
+vnet_spoke2_name="vnet-spoke2"
+vnet_spoke2_address_prefix="10.2.0.0/22"
+vnet_spoke2_front_subnet_name="snet-aks"
+vnet_spoke2_front_subnet_address_prefix="10.2.0.0/24"
+
 # Login and set correct context
 # Note: You can skip if using cloud shell
 # Command: VAR-1
--- 01-network.sh
+++ 01-network.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-vnetid=$(az network vnet create -g $resourceGroupName --name $vnetName \
-  --address-prefix 10.0.0.0/8 \
+######################
+#  _   _       _
+# | | | |_   _| |__
+# | |_| | | | | '_ \
+# |  _  | |_| | |_) |
+# |_| |_|\__,_|_.__/
+######################
+
+vnet_hub_id=$(az network vnet create -g $resource_group_name --name $vnet_hub_name \
+  --address-prefix $vnet_hub_address_prefix \
   --query newVNet.id -o tsv)
-echo $vnetid
+echo $vnet_hub_id
 
-subnetaksid=$(az network vnet subnet create -g $resourceGroupName --vnet-name $vnetName \
-  --name $subnetAks --address-prefixes 10.2.0.0/24 \
+vnet_hub_management_subnet_id=$(az network vnet subnet create -g $resource_group_name --vnet-name $vnet_hub_name \
+  --name $vnet_hub_management_subnet_name --address-prefixes $vnet_hub_management_subnet_address_prefix \
+  --delegations "Microsoft.ContainerInstance/containerGroups" \
   --query id -o tsv)
-echo $subnetaksid
+echo $vnet_hub_management_subnet_id
 
-subnetstorageid=$(az network vnet subnet create -g $resourceGroupName --vnet-name $vnetName \
-  --name $subnetStorage --address-prefixes 10.3.0.0/24 \
+vnet_hub_bastion_subnet_id=$(az network vnet subnet create -g $resource_group_name --vnet-name $vnet_hub_name \
+  --name $vnet_hub_bastion_subnet_name --address-prefixes $vnet_hub_bastion_subnet_address_prefix \
   --query id -o tsv)
-echo $subnetstorageid
-
-# Delegate a subnet to Azure NetApp Files
-# https://docs.microsoft.com/en-us/azure/azure-netapp-files/azure-netapp-files-delegate-subnet
-subnetnetappid=$(az network vnet subnet create -g $resourceGroupName --vnet-name $vnetName \
-  --name $subnetNetApp --address-prefixes 10.4.0.0/28 \
-  --delegations "Microsoft.NetApp/volumes" \
-  --query id -o tsv)
-echo $subnetnetappid
+echo $vnet_hub_bastion_subnet_id
