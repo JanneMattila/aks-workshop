911a2e52ffd77f2a8436cb668e6c3c70fe178a24
Janne Mattila <janne.mattila@hotmail.fi>
2022-04-11 19:19:24 +0300

    Add monitoring example

--- 09-monitoring.sh
+++ 09-monitoring.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-# TBA: https://github.com/JanneMattila/playground-aks-monitoring
+# Apply log and prometheus data collection settings 
+kubectl apply -f container-azm-ms-agentconfig.yaml
+
+# Read more about different configuration options:
+# https://github.com/microsoft/Docker-Provider/blob/ci_dev/kubernetes/container-azm-ms-agentconfig.yaml
+
+kubectl apply -f monitoring-app/
+
+# More information: 
+# https://github.com/JanneMattila/playground-aks-monitoring
