79c881bae7f2c7eb16b9f12278d21b1e335a8217
Janne Mattila <janne.mattila@hotmail.fi>
2022-04-13 11:32:42 +0300

    Add content for monitoring

--- 10-monitoring.sh
+++ 10-monitoring.sh
@@ -8,5 +8,9 @@ kubectl apply -f others/container-azm-ms-agentconfig.yaml
 
 kubectl apply -f monitoring-app/
 
+# Use Container Insights.
+
+# Use Log Analytics workspace and learn KQL queries.
+
 # More information: 
 # https://github.com/JanneMattila/playground-aks-monitoring
