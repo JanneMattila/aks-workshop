fe0816a97f02c3ab9fea866e4027cb1ef191be74
Janne Mattila <janne.mattila@hotmail.fi>
2022-04-14 07:29:15 +0300

    Add upgrade example

---
+++ 11-upgrade.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# See all available Kubernetes versions
+# Command: UPGRADE-1
+az aks get-versions -l $location -o table
+
+# See upgrades available for our cluster
+# Command: UPGRADE-2
+az aks get-upgrades -g $resource_group_name -n $aks_name -o table
+
+# See upgrades available for our nodepool
+# Command: UPGRADE-3
+az aks nodepool get-upgrades --nodepool-name $aks_nodepool1 -g $resource_group_name --cluster-name $aks_name -o table
+
+# Update max surge for an existing node pool
+# Note: For production node pools, we recommend a max_surge setting of 33%
+# Command: UPGRADE-4
+az aks nodepool update -n nodepool1 -g $resource_group_name --cluster-name $aks_name --max-surge 1
+
+# Options:
+# 1. Upgrade is steps: First control plane and then nodepools one-by-one
+# 2. Let AKS manage the upgrade according to this:
+#    https://docs.microsoft.com/en-us/azure/aks/upgrade-cluster#upgrade-an-aks-cluster
+
+# Option 1: Upgrade is steps
+# Upgrade only control plane
+# Command: UPGRADE-5
+az aks upgrade -g $resource_group_name -n $aksName --kubernetes-version 1.22.6 --control-plane-only --yes
+
+# Upgrade nodepool
+# Command: UPGRADE-6
+az aks nodepool upgrade --name $aks_nodepool1 -g $resource_group_name --cluster-name $aks_name --kubernetes-version 1.22.6
+
+# Option 2: Let AKS manage the upgrade
+# Command: UPGRADE-7
+az aks upgrade -g $resource_group_name -n $aks_name --kubernetes-version 1.22.6 --yes
+
+#
+# More information in here:
+# https://github.com/JanneMattila/playground-aks-maintenance
+#
