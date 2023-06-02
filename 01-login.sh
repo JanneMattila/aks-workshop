101aa8ba423c3ae049053a6dc3c95be98c241a1c
Janne Mattila <janne.mattila@hotmail.fi>
2023-06-02 08:13:59 +0300

    Add bastion extension update

--- 01-login.sh
+++ 01-login.sh
@@ -38,4 +38,5 @@ az group create -l $location -n $resource_group_name -o table
 # Prepare extensions and providers
 # Command: VAR-4
 az extension add --upgrade --yes --name aks-preview
+az extension add --upgrade --yes --name bastion
 az extension add --upgrade --yes --name ssh
