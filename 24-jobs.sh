df15f90b0b2bd816f789dfc80a42ade3586b480d
Janne Mattila <janne.mattila@hotmail.fi>
2023-06-21 07:47:02 +0300

    Add job example

--- 24-jobs.sh
+++ 24-jobs.sh
@@ -1,3 +1,22 @@
+####################
+#    _       _     
+#   (_) ___ | |__  
+#   | |/ _ \| '_ \ 
+#   | | (_) | |_) |
+#  _/ |\___/|_.__/ 
+# |__/        
+####################
+
+kubectl apply -f others/echo-job.yaml
+
+kubectl get job
+kubectl get pod
+
+job_app_pod1=$(kubectl get pod -o name | tail -n 1)
+echo $job_app_pod1
+
+kubectl logs $job_app_pod1
+
 #########################################
 #                        _       _     
 #   ___ _ __ ___  _ __  (_) ___ | |__  
@@ -18,3 +37,19 @@ echo $cronjob_app_pod1
 kubectl logs $cronjob_app_pod1
 
 kubectl delete -f others/echoer-cronjob.yaml
+
+# QUESTION:
+# ---------
+# What is the difference between a cronjob and a job?
+#
+# https://kubernetes.io/docs/concepts/workloads/controllers/job/
+# https://kubernetes.io/docs/concepts/workloads/controllers/cron-jobs/
+#
+
+# QUESTION:
+# ---------
+# Why do you see job pods in the output of "kubectl get pod"?
+# How long do they stay there?
+#
+# Hint: Study properties of the job
+kubectl describe cronjob echoer-cronjob
