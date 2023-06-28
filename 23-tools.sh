4869fe882a55c8dcf196372119d8833182f14471
Janne Mattila <janne.mattila@hotmail.fi>
2023-06-28 14:27:02 +0300

    Fix exclusion filename

--- 23-tools.sh
+++ 23-tools.sh
@@ -3,7 +3,7 @@
 # k9s
 download_k9s=$(curl -sL https://api.github.com/repos/derailed/k9s/releases/latest | jq -r '.assets[].browser_download_url' | grep k9s_Linux_amd64.tar.gz)
 wget $download_k9s -O k9s.tar.gz
-tar -xzf k9s.tar.gz --exclude='LICENSE' --exclude='READ.md'
+tar -xzf k9s.tar.gz --exclude='LICENSE' --exclude='README.md'
 ls -lF
 
 ./k9s
