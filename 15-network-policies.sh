ec8c07d49566b8da2add20e3300fd27ad348007a
Janne Mattila <janne.mattila@hotmail.fi>
2024-04-12 13:18:14 +0300

    Update demo

--- 15-network-policies.sh
+++ 15-network-policies.sh
@@ -27,7 +27,7 @@
 # Network Security Groups (NSG)?
 #
 
-kubectl apply -f others/network-app2.yaml
+kubectl apply -f others/network-policy/network-app2.yaml
 
 network_app2_pod1=$(kubectl get pod -n network-app2 -o name | head -n 1)
 echo $network_app2_pod1
@@ -38,14 +38,34 @@ echo $network_app2_pod1_ip
 network_app2_external_svc_ip=$(kubectl get service network-app2-external-svc -n network-app2 -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
 echo $network_app2_external_svc_ip
 
-network_app2_internal_svc_ip=$(kubectl get service network-app2-internal-svc -n network-app2 -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+network_app2_internal_svc_ip=$(kubectl get service network-app2-internal-svc -n network-app2 -o jsonpath="{.spec.clusterIP}")
 echo $network_app2_internal_svc_ip
 
 curl $network_app2_external_svc_ip
 
 # Full access between namespaces
 curl -X POST --data "HTTP GET \"http://network-app-internal2-svc.network-app2.svc.cluster.local\"" "$network_app_external_svc_ip/api/commands"
-curl -X POST --data "HTTP GET \"http://network-app-internal-svc.network-app.svc.cluster.local\"" "$network_app2_external_svc_ip/api/commands"
+curl -X POST --data "HTTP GET \"http://echo-app-svc.echo-app.svc.cluster.local/api/echo\"" "$network_app_external_svc_ip/api/commands"
 
-az aks update --help
-az aks update -g $resource_group_name -n $aks_name --network-policy azure
+curl -X POST --data "HTTP GET \"http://network-app-internal-svc.network-app.svc.cluster.local\"" "$network_app2_external_svc_ip/api/commands"
+curl -X POST --data "HTTP GET \"http://echo-app-svc.echo-app.svc.cluster.local/api/echo\"" "$network_app2_external_svc_ip/api/commands"
+
+curl -X POST --data "HTTP GET \"http://$network_app2_pod1_ip:8080\"" "$network_app_external_svc_ip/api/commands"
+curl -X POST --data "HTTP GET \"http://$network_app_pod1_ip:8080\"" "$network_app2_external_svc_ip/api/commands"
+
+# Test TCP connection to specific IP
+curl -X POST --data "NSLOOKUP bing.com" "$network_app2_external_svc_ip/api/commands"
+curl -X POST --data "TCP 13.107.21.200 80" "$network_app2_external_svc_ip/api/commands"
+curl -X POST --data "TCP 13.107.21.200 443" "$network_app2_external_svc_ip/api/commands"
+
+# Apply network policy
+kubectl apply -f others/network-policy/network-policy.yaml
+
+# Few points to understand:
+# - If you have service with port 80 and targetPort 8080, then you must allow 8080 in the rules.
+# - 
+# Tools:
+# https://artturik.github.io/network-policy-viewer/
+# https://orca.tufin.io/netpol/
+# https://editor.networkpolicy.io - Cillium
+#
