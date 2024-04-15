2920602ac6574d6e69545122f9cc8a103acb9012
Janne Mattila <janne.mattila@hotmail.fi>
2024-04-15 11:51:25 +0300

    Update example

--- 08-application-routing.sh
+++ 08-application-routing.sh
@@ -17,4 +17,23 @@ echo $ingress_ip2
 curl $ingress_ip2
 # -> Hello there!
 
+#########################
+# Internal only ingress
+#########################
+# Remove existing public ingress
+kubectl delete -f others/app-routing/02-ingress.yaml
+
+# Deploy internal ingress
+kubectl apply -f others/app-routing/internal/
+
+kubectl get ingress -n network-app
+
+kubectl describe ingress network-app-ingress3 -n network-app
+private_ingress_ip=$(kubectl get ingress network-app-ingress3 -n network-app -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+store_variable "private_ingress_ip"
+echo $private_ingress_ip
+
+# We cannot directly test this so let's use our web app network tester
+curl -X POST --data "HTTP GET \"http://$private_ingress_ip\"" "$network_app_external_svc_ip/api/commands"
+
 az aks approuting disable -g $resource_group_name -n $aks_name
