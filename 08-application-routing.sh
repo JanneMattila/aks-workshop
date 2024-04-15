46dc309231da4b4f63f4017f11c4f4c8e9f50171
Janne Mattila <janne.mattila@hotmail.fi>
2024-04-15 08:55:19 +0300

    Rename ingress

--- 08-application-routing.sh
+++ 08-application-routing.sh
@@ -9,12 +9,12 @@ kubectl apply -f others/app-routing/
 kubectl get service -n network-app
 kubectl get ingress -n network-app
 
-kubectl describe ingress network-app-ingress -n network-app
-ingress_ip=$(kubectl get ingress -n network-app -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")
-store_variable "ingress_ip"
-echo $ingress_ip
+kubectl describe ingress network-app-ingress2 -n network-app
+ingress_ip2=$(kubectl get ingress network-app-ingress2 -n network-app -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+store_variable "ingress_ip2"
+echo $ingress_ip2
 
-curl $ingress_ip
+curl $ingress_ip2
 # -> Hello there!
 
 az aks approuting disable -g $resource_group_name -n $aks_name
