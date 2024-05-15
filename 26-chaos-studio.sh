2156af41214933b9505332505c5b8b18ef7f41b3
Janne Mattila <janne.mattila@hotmail.fi>
2024-05-15 11:57:23 +0300

    Add Chaos Studio example

---
+++ 26-chaos-studio.sh
@@ -0,0 +1,36 @@
+# Install
+helm repo add chaos-mesh https://charts.chaos-mesh.org
+helm repo update
+kubectl create ns chaos-testing
+helm install chaos-mesh chaos-mesh/chaos-mesh --namespace=chaos-testing --set chaosDaemon.runtime=containerd --set chaosDaemon.socketPath=/run/containerd/containerd.sock
+
+kubectl get pods -n chaos-testing
+
+# DNS Chaos:
+# https://chaos-mesh.org/docs/simulate-dns-chaos-on-kubernetes/#configuration-description
+# {"action":"error","mode":"all","patterns":["bing.com","chaos-mesh.*","github.?om","login.microsoftonline.com"],"selector":{"namespaces":["network-app","network-app2","update-app"]}}
+
+curl -X POST --data "IPLOOKUP github.com" "$network_app_external_svc_ip/api/commands"
+curl -X POST --data "IPLOOKUP bing.com" "$network_app_external_svc_ip/api/commands"
+curl -X POST --data "IPLOOKUP login.microsoftonline.com" "$network_app_external_svc_ip/api/commands"
+curl -X POST --data "IPLOOKUP microsoft.com" "$network_app_external_svc_ip/api/commands"
+
+# Pod Chaos:
+# https://chaos-mesh.org/docs/simulate-pod-chaos-on-kubernetes/#field-description
+# {"action":"pod-failure","mode":"fixed-percent","value":"50","duration":"300s","selector":{"namespaces":["network-app","network-app2","update-app"]}}
+
+kubectl get pods -n network-app
+kubectl get pods -n update-app
+
+curl $network_app_external_svc_ip
+
+curl -s $update_app_svc_ip/api/update | jq .
+
+while true; do
+    curl -s $update_app_svc_ip/api/update | jq .
+done
+
+# Availability zone chaos:
+kubectl get nodes
+kubectl get pods -n update-app
+list_pods update-app
