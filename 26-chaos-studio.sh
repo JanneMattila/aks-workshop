9fe00454cad12bc50df19caa7292fb49c7249806
Janne Mattila <janne.mattila@hotmail.fi>
2025-11-08 10:08:27 +0200

    Update Chaos Studio demo

--- 26-chaos-studio.sh
+++ 26-chaos-studio.sh
@@ -9,12 +9,26 @@ helm repo add chaos-mesh https://charts.chaos-mesh.org
 helm repo update
 kubectl create ns chaos-testing
 helm install chaos-mesh chaos-mesh/chaos-mesh --namespace=chaos-testing --set chaosDaemon.runtime=containerd --set chaosDaemon.socketPath=/run/containerd/containerd.sock
+kubectl get deployments -n chaos-testing
+kubectl get service -n chaos-testing
+kubectl get daemonsets -n chaos-testing
+kubectl describe ds chaos-daemon -n chaos-testing
 
-kubectl get pods -n chaos-testing
+# To troubleshoot:
+kubectl logs -n chaos-testing -l app.kubernetes.io/component=controller-manager
+
+# Connect to Chaos Dashboard
+kubectl apply -f others/chaos-studio.yaml
+kubectl create token account-cluster-manager-cmicp
+kubectl port-forward -n chaos-testing svc/chaos-dashboard 2333:2333
 
 # DNS Chaos:
 # https://chaos-mesh.org/docs/simulate-dns-chaos-on-kubernetes/#configuration-description
-# {"action":"error","mode":"all","patterns":["bing.com","chaos-mesh.*","github.?om","login.microsoftonline.com","network-app-internal-svc.*","network-app-clusterip-svc.*"],"selector":{"namespaces":["network-app","network-app2","update-app"]}}
+# {"action":"error","mode":"all","patterns":["bing.com","chaos-mesh.*","github.?om","login.microsoftonline.com","network-app-internal-svc.*","network-app-clusterip-svc.*","sql*"],"selector":{"namespaces":["network-app","network-app2","update-app","sql-app"]}}
+# Network Chaos:
+# https://chaos-mesh.org/docs/simulate-network-chaos-on-kubernetes/#
+# {"action":"loss","target":{"mode":"all"},"selector":{"namespaces":["network-app","network-app2","update-app","sql-app"]},"mode":"all","direction":"to","loss":{"correlation":"100","loss":"100"},"externalTargets":["microsoft.com"]}
+# {"action":"partition","target":{"mode":"all"},"selector":{"namespaces":["network-app","network-app2","update-app","sql-app"]},"mode":"all","direction":"to","externalTargets":["microsoft.com"]}
 
 curl -X POST --data "IPLOOKUP github.com" "$network_app_external_svc_ip/api/commands"
 curl -X POST --data "IPLOOKUP bing.com" "$network_app_external_svc_ip/api/commands"
@@ -33,6 +47,7 @@ curl -X POST --data "HTTP GET https://login.microsoftonline.com" "$network_app_e
 curl -X POST --data "HTTP GET http://network-app-internal-svc" "$network_app_external_svc_ip/api/commands"
 curl -X POST --data "HTTP GET http://network-app-clusterip-svc" "$network_app_external_svc_ip/api/commands"
 curl -X POST --data "HTTP GET https://microsoft.com" "$network_app_external_svc_ip/api/commands"
+curl -X POST --data "HTTP GET https://github.com" "$network_app_external_svc_ip/api/commands"
 
 # Pod Chaos:
 # https://chaos-mesh.org/docs/simulate-pod-chaos-on-kubernetes/#field-description
@@ -43,6 +58,7 @@ kubectl get pods -n network-app
 list_pods network-app
 
 kubectl get pods -n update-app
+list_pods update-app
 
 curl $network_app_external_svc_ip
 
@@ -53,6 +69,25 @@ while true; do
 done
 
 # Availability zone chaos:
+kubectl apply -f others/chaos-studio/storage-app-lrs.yaml
+kubectl apply -f others/chaos-studio/storage-app-zrs.yaml
+kubectl get storageclass default -o yaml # ZRS
+kubectl get storageclass managed-csi-premium-lrs-sc -o yaml # LRS
+
+kubectl get svc -n storage-app-lrs
+kubectl get svc -n storage-app-zrs
+
 kubectl get nodes
-kubectl get pods -n update-app
-list_pods update-app
+kubectl get nodes -o custom-columns=NAME:'{.metadata.name}',REGION:'{.metadata.labels.topology\.kubernetes\.io/region}',ZONE:'{metadata.labels.topology\.kubernetes\.io/zone}'
+
+kubectl get pods -n storage-app-lrs
+kubectl get pods -n storage-app-zrs
+
+# Split view:
+list_pods storage-app-lrs # Zone: x --> x
+list_pods storage-app-zrs # Zone: x --> y
+
+kubectl get pods -n storage-app-lrs -w
+kubectl get pods -n storage-app-zrs -w
+
+kubectl describe pods -n storage-app-lrs
